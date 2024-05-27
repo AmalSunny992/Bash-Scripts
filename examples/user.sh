70ac43a17839d8bd18b0af841046725426e57c79
AmalSunny992 <amalsunny992@gmail.com>
2024-05-27 09:39:45 +0530

    Update user.sh

--- examples/user.sh
+++ examples/user.sh
@@ -3,10 +3,24 @@
 echo "======================Create a new user==============================="
 echo " "
 read -p "Enter a username : " username
+count=1
+while [$pswd -ne $pswd2]; do
+  count=$((count+1))
+  read -p "Enter a Password : " pswd
+  read -p "Enter Password again : " pswd2
+  echo "password mismatch, Try again"
+  if[count -eq 5]
+    echo "maximum try exceeded...exiting"
+    exit
+  fi
+  done
+password=$pswd
+sudo useradd "$username"
+echo "$username:$password" | sudo chpasswd
 echo " "
 echo "====================Add new user to a group==========================="
 echo " "
 read -p "Enter the group to add '$username' to : " group
 echo " "
-useradd "$username"
 usermod "$group" "$username"
+echo "User $USERNAME created."
