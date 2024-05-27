6b9d6cd547017bb8786d6725adf9a3363c68eeac
AmalSunny992 <amalsunny992@gmail.com>
2024-05-26 20:46:31 -0600

    Update archive.sh

--- examples/archive.sh
+++ examples/archive.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+echo "Enter the source directory path for Archiving : "
+input " "
+echo "Enter the source directory path for Archiving : "
+echo "Enter the filename to be used "
 filename=$(date +"%Y%m%d%Hh%Mm%Ss")
-dest_dir="/home/devops/backup"
 source_dir=$(pwd)
+dest_dir="/home/devops/backup"
 
 tar -czf "$dest_dir/$filename.tar.gz" "$source_dir"
 echo "Archive created: $dest_dir/$filename.tar.gz"
