f5f45ba372602f657d8268d27e9a118632f1dd5c
Amal Sunny <amalsunny992@gmail.com>
2024-06-06 00:18:12 +0530

    Update restart-service.sh

--- examples/restart-service.sh
+++ examples/restart-service.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-echo "Enter Service name to restart : " SERVICE_NAME
+read -p "Enter Service name to restart: " SERVICE_NAME
 
-if systemctl restart $SERVICE_NAME; then
+# Check if the service name is not empty
+if [ -z "$SERVICE_NAME" ]; then
+  echo "No service name provided."
+  exit 1
+fi
+
+# Attempt to restart the service
+if systemctl restart "$SERVICE_NAME"; then
   echo "$SERVICE_NAME restarted successfully."
 else
   echo "Failed to restart $SERVICE_NAME."
